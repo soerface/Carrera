12f2c49930e2a1046cca373bda0261f689a128db
Sören <wegener92@googlemail.com>
2011-08-17 20:43:28 +0200

    Added class which will contain devices (UE9).

---
+++ devices.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module contains the devices to communicate with the track."""
+
+import ue9
+
+class UE9(object):
+
+    def __init__():
+        self.device = ue9.UE9()
+
+    @property
+    def player(self, num):
+        pass
+
+    #def traffic_light(self):
+    #    pass
