7963195f6e22c5b8800635569e2a90c55be8387a
Sören <wegener92@googlemail.com>
2011-08-19 11:07:52 +0200

    Added method to query the track.

--- devices.py
+++ devices.py
@@ -4,7 +4,7 @@ import ue9
 
 class UE9(object):
 
-    def __init__():
+    def __init__(self):
         self.device = ue9.UE9()
 
     @property
@@ -13,3 +13,9 @@ class UE9(object):
 
     #def traffic_light(self):
     #    pass
+
+    def track_state(self, num):
+        feedback = self.device.feedback(EIOMask=255, EIODir=0)
+        state = feedback['EIOState']
+        return [not state & 0b1, not state & 0b10,
+                not state & 0b100, not state & 0b1000]
