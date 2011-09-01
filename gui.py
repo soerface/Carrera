74ca5dc3b113baa31185c1d514f8171f4f6af8e3
Sören <wegener92@googlemail.com>
2011-09-01 16:18:23 +0200

    Renamed eventhandler methods.

--- gui.py
+++ gui.py
@@ -10,7 +10,8 @@ class Carrera(object):
         self.builder.add_from_file('gui.glade')
         self.builder.connect_signals(self)
         self.num_players = 0
-        self.add_player()
+        for i in range(2):
+            self.add_player()
         gamemodes = gtk.combo_box_new_text()
         for text in GAMEMODES:
             gamemodes.append_text(text)
@@ -27,12 +28,12 @@ class Carrera(object):
     def quit(self, *args):
         gtk.main_quit()
 
-    def add_player(self, *args):
+    def add_player(self):
         if self.num_players == 4:
             return
         box = gtk.HBox()
         button = gtk.Button()
-        button.connect('clicked', self.remove_player)
+        button.connect('clicked', self.on_remove_player_clicked)
         entry = gtk.Entry()
         box.add(entry)
         box.add(button)
@@ -42,11 +43,18 @@ class Carrera(object):
         box.show()
         self.num_players += 1
 
-    def remove_player(self, obj):
+    def on_add_player_clicked(self, obj):
+        self.add_player()
+
+    def on_remove_player_clicked(self, obj):
         row = obj.parent
         box = row.parent
         box.remove(row)
         self.num_players -= 1
+
+    def on_start_race_clicked(self, obj):
+        self.builder.get_object('main').hide()
+        self.builder.get_object('race').show()
 
 if __name__ == '__main__':
     carrera = Carrera()
