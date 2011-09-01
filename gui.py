00a084a793682684abc8de133928822621bd76c7
Sören <wegener92@googlemail.com>
2011-09-01 14:41:07 +0200

    Continued with user interface, event handling, buttons...

--- gui.py
+++ gui.py
@@ -2,12 +2,21 @@
 
 import gtk
 
+GAMEMODES = ['Match']
 class Carrera(object):
 
     def __init__(self):
         self.builder = gtk.Builder()
         self.builder.add_from_file('gui.glade')
         self.builder.connect_signals(self)
+        self.num_players = 0
+        self.add_player()
+        gamemodes = gtk.combo_box_new_text()
+        for text in GAMEMODES:
+            gamemodes.append_text(text)
+        gamemodes.set_active(0)
+        self.builder.get_object('modes_box').add(gamemodes)
+        gamemodes.show()
 
     def run(self):
         try:
@@ -17,6 +26,14 @@ class Carrera(object):
 
     def quit(self):
         gtk.main_quit()
+
+    def add_player(self, *args):
+        if self.num_players == 4:
+            return
+        self.num_players += 1
+        box = gtk.combo_box_new_text()
+        self.builder.get_object('player_box').add(box)
+        box.show()
 
 if __name__ == '__main__':
     carrera = Carrera()
